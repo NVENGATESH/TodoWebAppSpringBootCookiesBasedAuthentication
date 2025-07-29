af9caee8353281677e3146a3f85f3a1c4b5adc81
NVENGATESH <vengatesh122003@gmail.com>
2025-07-29 20:15:44 +0530

    newupdates

--- src/main/java/com/nisahnth/ToDoListWebApp/config/CorsConfig .java
+++ src/main/java/com/nisahnth/ToDoListWebApp/config/CorsConfig .java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:5173/") // replace with your actual frontend URL
+            .allowedOrigins("http://localhost:5173") // replace with your actual frontend URL
             .allowedMethods("*")
             .allowedHeaders("*")
             .allowCredentials(true)
